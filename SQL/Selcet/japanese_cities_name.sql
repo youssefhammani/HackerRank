c47fd0583353a961f8c70856046d37f63a665ea4
yhammani <youssefhammani04@gmail.com>
2022-11-24 20:34:55 +0100

    slct by name

---
+++ SQL/Selcet/japanese_cities_name.sql
@@ -0,0 +1,20 @@
+/*
+Problem : Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+The CITY table is described as follows:
+
+            CITY
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
+
+-- Solution : MySQL
+
+select name from city where countrycode = 'JPN';
