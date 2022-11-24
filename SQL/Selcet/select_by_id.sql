dd29d721e1d4763119cfab3c5e0eeed8a25af146
yhammani <youssefhammani04@gmail.com>
2022-11-24 20:09:25 +0100

    slct by id

---
+++ SQL/Selcet/select_by_id.sql
@@ -0,0 +1,20 @@
+/*
+Problem : Query all columns for a city in CITY with the ID 1661.
+
+The CITY table is described as follows:
+
+            CITY
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
+
+-- Solution : MySQL
+
+select * from city where id = 1661;
