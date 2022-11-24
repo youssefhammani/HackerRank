ee6727b69b68ab4ed1e806f2b3fc561683d614f8
yhammani <youssefhammani04@gmail.com>
2022-11-24 20:27:28 +0100

    slct all by condition

---
+++ SQL/Selcet/japanese_cities_attributes.sql
@@ -0,0 +1,20 @@
+/*
+Problem : Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+The CITY table is described as follows:
+
+            CITY
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(3)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+*/
+
+-- Solution : MySQL
+
+select * from city where countrycode = 'JPN' ;
